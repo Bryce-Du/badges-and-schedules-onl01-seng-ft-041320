055647cfded49d858da641ff328ef1300747560c
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-14 15:19:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms (speakers)
   room_assignments = []
   speakers.each_with_index do |speaker, index|
-    room_assignments << "Hello #{speaker}! You'll be assigned to room #{index + 1}!"
+    room_assignments << "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
   end
   return room_assignments
 end
