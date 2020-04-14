baa082b33390d5f87f4af7de3fe278a3f6caee81
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-14 15:15:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,4 +3,16 @@
 def badge_maker (name)
   return "Hello, my name is #{name}"
 end
-def badge
+def batch_badge_creator (names)
+  badges = []
+  names.each do |name|
+    badges << badge_maker(name)
+  end
+  return badges
+end
+def assign_rooms (speakers)
+  room_assignments = []
+  speakers.each do |speaker|
+    room_assignments << "Hello #{speaker}! You'll be assigned to room "
+  end
+end
